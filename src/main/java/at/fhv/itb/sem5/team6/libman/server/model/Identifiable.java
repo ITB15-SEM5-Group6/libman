0f5251c68b4a1f43bb3b25444ee48514d13feeeb
Alex <alex.dengg16@gmail.com>
2017-11-02 17:06:25 +0100

    updated models and DTOs

--- src/main/java/at/fhv/itb/sem5/team6/libman/server/model/Identifiable.java
+++ src/main/java/at/fhv/itb/sem5/team6/libman/server/model/Identifiable.java
@@ -2,7 +2,7 @@ package at.fhv.itb.sem5.team6.libman.server.model;
 
 import java.io.Serializable;
 
-public interface Identifyable<T extends Serializable> {
+public interface Identifiable<T extends Serializable> {
     T getId();
 
     void setId(T id);
