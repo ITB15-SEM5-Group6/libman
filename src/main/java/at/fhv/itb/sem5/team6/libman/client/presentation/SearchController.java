913ebe4d01d93cd6a3a3165ecc80c10d09c3ce5a
cts6638 <Apsonere2015>
2017-11-05 19:08:47 +0100

    added Logo

--- src/main/java/at/fhv/itb/sem5/team6/libman/client/presentation/SearchController.java
+++ src/main/java/at/fhv/itb/sem5/team6/libman/client/presentation/SearchController.java
@@ -1,5 +1,7 @@
 package at.fhv.itb.sem5.team6.libman.client.presentation;
 
+import at.fhv.itb.sem5.team6.libman.shared.interfaces.ILibraryFactory;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
@@ -7,7 +9,14 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.List;
+
 public class SearchController {
+    static String host = "localhost";
+    static int port = 1099;
+
 
     @FXML
     private AnchorPane anchorPane;
@@ -49,7 +58,20 @@ public class SearchController {
         columnMediatype.prefWidthProperty().bind(tableView.widthProperty().divide(4)); // w * 1/2
         columnAvailable.prefWidthProperty().bind(tableView.widthProperty().divide(4)); // w * 1/4
 
-
     }
+
+    @FXML
+    void search(ActionEvent event) {
+        try {
+            Registry registry = LocateRegistry.getRegistry(host,port);
+            ILibraryFactory stub = (ILibraryFactory) registry.lookup("LibraryFactory");
+            stub.create().findAllMedia().forEach(System.out::println);
+
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+
 
 }
