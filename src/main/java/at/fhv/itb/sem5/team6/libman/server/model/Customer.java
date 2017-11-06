36798e21e27dc0943efe6aad313bf790d1b22897
i7Gamer <timorzipa@gmail.com>
2017-11-06 09:10:17 +0100

    Merge branch 'master' of /Users/timorzipa/IdeaProjects/libman with conflicts.

--- src/main/java/at/fhv/itb/sem5/team6/libman/server/model/Customer.java
+++ src/main/java/at/fhv/itb/sem5/team6/libman/server/model/Customer.java
@@ -1,20 +1,22 @@
 package at.fhv.itb.sem5.team6.libman.server.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import at.fhv.itb.sem5.team6.libman.shared.DTOs.mutable.MutableCustomer;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 
+//Lombok annotations to reduce boilerplate (lombok plugin has to be installed in IDE)
+@Data // applies lombok-annotations
+@Getter // enables getters
+@Setter // enables setters
+@AllArgsConstructor // creates a constructor accepting all args in the order the attributes are defined
+@NoArgsConstructor // creates an empty constructor
+
+//MongoDb annnotation
 @Document
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-public class Customer implements Serializable{
+public class Customer implements MutableCustomer, Serializable{
     @Id
     private String id;
 
--- src/main/java/at/fhv/itb/sem5/team6/libman/server/model/Reservation.java
+++ src/main/java/at/fhv/itb/sem5/team6/libman/server/model/Reservation.java
@@ -1,25 +1,39 @@
 package at.fhv.itb.sem5.team6.libman.server.model;
 
-import lombok.*;
+import at.fhv.itb.sem5.team6.libman.shared.DTOs.immutable.ImmutableCustomer;
+import at.fhv.itb.sem5.team6.libman.shared.DTOs.immutable.ImmutableMedia;
+import at.fhv.itb.sem5.team6.libman.shared.DTOs.mutable.MutableReservation;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serializable;
+
 //Lombok annotations to reduce boilerplate (lombok plugin has to be installed in IDE)
 @Data // applies lombok-annotations
-@Getter // enables getters
-@Setter // enables setters
-@AllArgsConstructor // creates a constructor accepting all args in the order the attributes are defined
-@NoArgsConstructor // creates an empty constructor
+@Getter
+@Setter
 
 //MongoDb annnotation
 @Document
-public class Reservation {
-
+public class Reservation implements MutableReservation, Identifiable<String>, Serializable {
     @Id
     private String id;
     @DBRef //MongoDb uses this object as a reference
-    private Media info;
+    private Media media;
     @DBRef //MongoDb uses this object as a reference
     private Customer customer;
+
+    @Override
+    public void setMedia(ImmutableMedia media) {
+        this.media = (Media) media;
+    }
+
+    @Override
+    public void setCustomer(ImmutableCustomer customer) {
+        this.customer = (Customer) customer;
+    }
 }
